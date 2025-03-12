e90c2ded38b8f625f918db25ace68040c369f103
CyberMedina <jhonatanmedina5255@gmail.com>
2025-03-12 00:11:50 -0600

    añadiendo yolov5 sin integracion completa

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,6 @@
 from app.routes.main_routes import bp as main_bp
 from app.routes.api_routes import bp as api_bp
+from app.routes.yolo_routes import yolo_bp
 
 def register_blueprints(app):
     """
@@ -8,5 +9,7 @@ def register_blueprints(app):
     app.logger.info("Registrando blueprints...")
     app.register_blueprint(main_bp)
     app.register_blueprint(api_bp)
+    app.register_blueprint(yolo_bp, url_prefix='/yolo')
     app.logger.info(f"Blueprint registrado: {main_bp.name} con prefijo: {main_bp.url_prefix or '/'}")
     app.logger.info(f"Blueprint registrado: {api_bp.name} con prefijo: {api_bp.url_prefix or '/'}")
+    app.logger.info(f"Blueprint registrado: yolo con prefijo: /yolo")
