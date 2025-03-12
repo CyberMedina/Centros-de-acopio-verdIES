056c20110e6e7632f9545b4fb3d163f578429720
CyberMedina <jhonatanmedina5255@gmail.com>
2025-03-11 22:41:20 -0600

    version antes de unificar con YoloV5

--- models/api.py
+++ models/api.py
@@ -1,33 +1,14 @@
 import requests
 from flask import jsonify
+from info_centro_acopio import *
 
 def obtener_detalle_centro_acopio(id):
-    # URL de la API
-    url = f"http://127.0.0.1:8000/api/v1/centroacopios/{id}"
+
+    return datos_centro_acopio
     
-    # Data que enviarías en el POST (si hay alguna)
-    data = {}  # Si no necesitas enviar data, deja esto vacío
-    
-    # Realizar el POST
-    response = requests.get(url, json=data)
-    
-    # Obtener el JSON de la respuesta
-    response_json = response.json()
-    
-    return response_json
+
+
 
 
 def obtener_clasificacion_residuos():
-    # URL de la API
-    url = "http://127.0.0.1:8000/api/v1/materiales"
-
-    # Data que enviarías en el POST (si hay alguna)
-    data = {}  # Si no necesitas enviar data, deja esto vacío
-
-    # Realizar el POST
-    response = requests.get(url, json=data)
-
-    # Obtener el JSON de la respuesta
-    response_json = response.json()
-
-    return response_json
+    return materiales_aceptados
